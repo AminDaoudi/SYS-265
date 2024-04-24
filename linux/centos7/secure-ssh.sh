a326887223be9423161f1f42ffb61724c225f1ea
AminDaoudi <amin.daoudi@mymail.champlain.edu>
2024-04-24 20:26:46 +0000

    script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,10 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes root ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
+
+useradd -m -d /home/"$1" -s /bin/bash "$1"
+sudo mkdir /home/"$1"/.ssh
+cp /home/deployer/gitlab/SYS-265/linux/public-keys/id_rsa.pub /home/"$1"/.ssh/authorized_keys
+chmod 700 /home/"$1"/.ssh
+chmod 600 /home/"$1"/.ssh/authorized_keys
+chown -R "$1":"$1" /home/"$1"/.ssh
